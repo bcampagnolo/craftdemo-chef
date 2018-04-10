0da97c35b2d205f1d0b84a525de1f3a726f8dcb0
Paul Morrison <pmorrison@classy.org>
2018-04-10 12:09:50 -0700

    clean up names

--- cookbooks/data-service/test/integration/dev/serverspec/dev_spec.rb
+++ cookbooks/data-service/test/integration/dev/serverspec/dev_spec.rb
@@ -5,35 +5,10 @@ require 'uri'
 # Required by serverspec
 set :backend, :exec
 
-users = [
-  { 'name' => 'jdeploy', 'uid' => 50_002 }
-]
-
-groups = [
-  { 'name' => 'jdeploy', 'gid' => 50_002 }
-]
-
-describe 'System settings' do
-  users.each do |user|
-    describe user(user['name']) do
-      it { should exist }
-      it { should have_uid user['uid'] }
-    end
-  end
-
-  groups.each do |group|
-    describe group(group['name']) do
-      it { should exist }
-      it { should have_gid group['gid'] }
-    end
-  end
-
-end
-
 
 describe 'flask service' do
   it 'is listening on port 5000' do
-    expect(port(9000)).to be_listening
+    expect(port(5000)).to be_listening
   end
   it 'has a running service of flask' do
     expect(service('flask')).to be_running
--- cookbooks/nginx/_aws/user-data.sh
+++ cookbooks/nginx/_aws/user-data.sh
@@ -1,17 +1,60 @@
 #!/bin/bash
 
-ARTIFACT_NAME="chef-12.22.1-1.el7.x86_64.rpm"
-S3_ARTIFACT_BUCKET_NAME="s3://496911069803-us-west-2-artifacts/rpm"
-DEST_DIR="/tmp/deploy"
+# ARTIFACT_NAME="chef-12.22.1-1.el7.x86_64.rpm"
+# S3_ARTIFACT_BUCKET_NAME="s3://496911069803-us-west-2-artifacts/rpm"
+# DEST_DIR="/tmp/deploy"
 
-# Create DEST_DIR
-mkdir -p "${DEST_DIR}" && cd "${DEST_DIR}" || exit 'Cannot cd to ${DEST_DIR}'
+# # Create DEST_DIR
+# mkdir -p "${DEST_DIR}" && cd "${DEST_DIR}" || exit 'Cannot cd to ${DEST_DIR}'
 
-# Download the artifact
-#aws s3 cp "${S3_ARTIFACT_BUCKET_NAME}/${ARTIFACT_NAME}" ${DEST_DIR}
+# # Download the artifact
+# #aws s3 cp "${S3_ARTIFACT_BUCKET_NAME}/${ARTIFACT_NAME}" ${DEST_DIR}
 
-# Rename artifact
-#mv ${ARTIFACT_NAME} shell-services.zip
-#rpm -ivh ${DEST_DIR}/${ARTIFACT_NAME}
+# # Rename artifact
+# #mv ${ARTIFACT_NAME} shell-services.zip
+# #rpm -ivh ${DEST_DIR}/${ARTIFACT_NAME}
 
-touch "${DEST_DIR}"/user_data.txt
+# touch "${DEST_DIR}"/user_data.txt
+
+# Temporarily override the default umask of 0077
+umask 0022
+
+# Set the default region for the AWS CLI/SDK
+echo export AWS_DEFAULT_REGION=${AWS::Region} > /etc/profile.d/aws_default_region.sh && . /etc/profile.d/aws_default_region.sh
+
+mkdir -p /tmp/deploy
+cd /tmp/deploy
+
+###### Download chef out of s3 ######s
+# 496911069803-us-west-2-artifacts/chef/cookbooks.zip
+aws s3 cp s3://496911069803-us-west-2-artifacts/chef/cookbooks.zip ${AWS::StackName}.zip
+
+if [ $? -ne 0 ]; then
+    # Unrecoverable error, so fail fast
+    cfn-signal --region ${AWS::Region} --resource AutoScalingGroup --stack ${AWS::StackName} --success false
+fi
+
+unzip -o ${AWS::StackName}.zip -d cookbooks
+
+# ###### Run Chef ######
+mkdir -p /var/log/chef
+
+cd cookbooks
+ls config/solo.rb
+ls nodes/web-services/us-west-2/dev.json
+
+chef-solo -c config/solo.rb -j nodes/web-services/us-west-2/dev.json -l debug --force-logger
+
+if [ $? -eq 0 ]; then
+    # All done!
+    cfn-signal --region ${AWS::Region} --resource AutoScalingGroup --stack ${AWS::StackName}
+else
+# Deploy failed upload logs and die
+INSTANCE_ID=`curl http://169.254.169.254/latest/meta-data/instance-id`
+curl -s http://169.254.169.254/latest/user-data > /var/log/user-data.log
+# flask-west2-prod-west2-logs/fail_deploys/
+for log in /var/log/chef/chef.log /var/log/userdata.log /var/log/boot.log /var/log/cloud-*.log /var/log/nginx/access.log /var/log/nginx/error.log; do
+    [ -f $log ] && aws s3 cp --acl bucket-owner-full-control $log s3://flask-west2-prod-west2-logs/fail_deploys/${AWS::StackName}/$INSTANCE_ID/`basename $log`
+done
+shutdown -h now
+fi
